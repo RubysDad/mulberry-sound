c09c5cfcd68bf9903f5f07c339579ecec720aa15
Rubysdad <markandtabithamorales@gmail.com>
2017-09-13 23:23:31 +0000

    added dashboard and template

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :podcasts
-  root 'pages#index'
   
   resources :podcasts, only: [:index, :show] do
     resources :episodes
   end
+  
+  authenticated :podcast do
+    root 'podcasts#dashboard', as: "authenticated_root"
+  end
+  
+  root 'pages#index'
 end
